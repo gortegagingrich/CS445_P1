401349742a2263953d72c4c8019301f7bcecc0c0
Gabriel <gortegagingrich@gmail.com>
2017-04-03 14:51:02 -0700

    Finished implementing all of the required drawing functionality. Next, I need to deal with file i/o.

--- src/CS445_P1.java
+++ src/CS445_P1.java
@@ -1,3 +1,16 @@
+/***************************************************************
+ *		file: CS445_P1.java
+ *		author: G. Ortega-Gingrich
+ *		class: CS 445 - Computer Graphics
+ * 
+ *		assignment: program 1
+ *		date last modified: 4/3/2017
+ * 
+ *		purpose: This program takes a filename from the command line,
+ *		parses its contents, and draws the lines, circles, and ellipses
+ *		described.
+ ****************************************************************/
+
 import java.util.ArrayList;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.input.Keyboard;
@@ -6,26 +19,17 @@ import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
 
-/**
- *
- * @author Gabriel
- */
 public class CS445_P1 {
-	
-	public static final float[][] COLORS = {
-		{1f, 0f, 0f}, // line
-		{0f, 1f, 0f}, // circle
-		{0f, 0f, 1f}  // ellipse
-	};
 
-	/**
-	 * @param args the command line arguments
-	 */
+	// method: main
+	// purpose: this static method is called when the program is run.
+	// It creates the window, initializses opengl, and contains the main loop
 	public static void main(String[] args) throws LWJGLException {
 		// TODO code application logic here
 		Display.setDisplayMode(new DisplayMode(640,480));
 		Display.setTitle("Project 1");
 		Display.create();
+		center();
 		
 		GL11.glPointSize(1);
 		GL11.glClearColor(0f,0f,0f,1f);
@@ -33,13 +37,13 @@ public class CS445_P1 {
 		GL11.glMatrixMode(GL11.GL_PROJECTION);
 		GL11.glLoadIdentity();
 		GL11.glOrtho(0, 640, 0, 480, 1, -1);
-		GL11.glMatrixMode(GL11.GL_MODELVIEW);
 		
 		ArrayList<Shape> shapes = new ArrayList<>();
 		
 		shapes.add(new Shape(200,50,100, 200, true));
 		shapes.add(new Shape(50,50,300, 300, true));
 		shapes.add(new Shape(500,300,70));
+		shapes.add(new Shape(225,370,35, 75, false));
 		
 		while (!Display.isCloseRequested() && !Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
 			
@@ -56,5 +60,16 @@ public class CS445_P1 {
 		}
 		
 		Display.destroy();
+	}
+	
+	// method: center
+	// purpose: this static method centers the window on the screen
+	private static void center() {
+		int scrWidth = Display.getDesktopDisplayMode().getWidth();
+		int scrHeight = Display.getDesktopDisplayMode().getHeight();
+		int width = Display.getWidth();
+		int height = Display.getHeight();
+		
+		Display.setLocation(scrWidth/2 - width/2, scrHeight/2 - height/2);
 	}
 }
--- src/Shape.java
+++ src/Shape.java
@@ -1,81 +1,70 @@
+/***************************************************************
+ *		file: Shape.java
+ *		author: G. Ortega-Gingrich
+ *		class: CS 445 - Computer Graphics
+ * 
+ *		assignment: program 1
+ *		date last modified: 4/3/2017
+ * 
+ *		purpose: This program takes a filename from the command line,
+ *		parses its contents, and draws the lines, circles, and ellipses
+ *		described.
+ ****************************************************************/
 
 import java.util.ArrayList;
 import org.lwjgl.opengl.GL11;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Gabriel
- */
 public class Shape {
 	
 	private static enum Type {LINE, CIRCLE, ELLIPSE};
 	
-	private Type type;
+	private final Type type;
 	ArrayList<float[]> points;
 	
-	// circles
+	// constructor: 3 floats
+	// purpose: creates a Shape instance of type circle with the given center x,
+	// center y, and radius
 	public Shape(float cX, float cY, float r) {
 		points = new ArrayList<>();
 		
-		for (float i = 0; i < 640; i++) {
-			for (float j = 0; j < 480; j++) {
-				if (Math.round(distance(i,j,cX,cY)) == r) {
-					points.add(new float[] {i,j});
-				}
-			}
-		}
+		makeEllipse(cX, cY, r, r);
 		
 		type = Type.CIRCLE;
 	}
 	
-	// lines and ellipses
-	public Shape(float x1, float y1, float x2, float y2, boolean line) {
+	// constructor: 4 floats
+	// purpose: creates a Shape instance of type ellipse, treating the first two
+	// floats as the center and the next two as the width and height, or of type
+	// line, treating the first two floats as point A and the next as point B
+	public Shape(float float0, float float1, float float2, float float3, boolean line) {
 		points = new ArrayList<>();
 		
 		if (line) { // makes it a line
-			float dX, dY;
-			float distance = distance(x1, y1, x2, y2);
-			
-			dX = (x2 - x1) / distance;
-			dY = (y2 - y1) / distance;
-			
-			float distanceMoved = 0;
-			
-			while (distanceMoved < distance) {
-				points.add(new float[] {x1, y1});
-				x1 += dX;
-				y1 += dY;
-				distanceMoved += Math.sqrt(dX * dX + dY * dY);
-			}
-			
-			System.out.printf("Distance: %f\ndX: %f\ndY: %f\n", distance, dX, dY);
+			makeLine(float0, float1, float2, float3);
 			
 			type = Type.LINE;
 		} else { // makes it an ellipse
-			
+			makeEllipse(float0, float1, float2, float3);
 			
 			type = Type.ELLIPSE;
 		}
 	}
 	
+	// method: draw
+	// purpose: This method sets the color to the type's corresponding color,
+	// and draws each predetermined pixel
 	public void draw() {
 		GL11.glBegin(GL11.GL_POINTS);
 		
 		switch (type) {
 			case LINE:
-				GL11.glColor3f(1f, 0f, 0f);
+				GL11.glColor3f(1, 0, 0);
 				break;
 			case ELLIPSE:
-				GL11.glColor3f(0f, 1f, 0f);
+				GL11.glColor3f(0, 1, 0);
 				break;
 			case CIRCLE:
-				GL11.glColor3f(0f, 0f, 1f);
+				GL11.glColor3f(0, 0, 1);
 		}
 		
 		for (float[] point: points) {
@@ -87,6 +76,45 @@ public class Shape {
 		GL11.glEnd();
 	}
 	
+	// method: makeEllipse
+	// purpose: this method adds every required point to create an ellipse with
+	// the given center, width, and height to the ArrayList points
+	private void makeEllipse(float cX, float cY, float width, float height) {
+		double circ, dir, dDir;
+		
+		dir = 0;
+		circ = 2 * Math.PI * Math.sqrt((width * width + height * height) / 2);
+		dDir = (2 * Math.PI) / circ / 2;
+		
+		while (dir <= (2 * Math.PI)) {
+			points.add(new float[] {cX + (float)(width * Math.cos(dir)),
+				cY + (float)(height * Math.sin(dir))});
+			dir += dDir;
+		}
+	}
+	
+	// method: makeLine
+	// purpose: this method adds every required point to create a line with the
+	// given endpoints to the ArrayList points
+	private void makeLine(float x1, float y1, float x2, float y2) {
+		float dX, dY;
+		float distance = distance(x1, y1, x2, y2);
+
+		dX = (x2 - x1) / distance;
+		dY = (y2 - y1) / distance;
+
+		float distanceMoved = 0;
+
+		while (distanceMoved <= distance) {
+			points.add(new float[] {x1, y1});
+			x1 += dX;
+			y1 += dY;
+			distanceMoved += Math.sqrt(dX * dX + dY * dY);
+		}
+	}
+	
+	// method: distance
+	// purpose: this method calculates the distance between two points
 	private float distance(float x1, float y1, float x2, float y2) {
 		return (float)Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2,2));
 	}
